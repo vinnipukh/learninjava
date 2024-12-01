74ac007975dc359c91cd2545ea38e1b91da859db
vinnipukh <oyunlarinefendiler@gmail.com>
2024-12-01 15:00:59 +0300

    1500

---
+++ src/tr/edu/ozyegin/cs101/geometry2d/Point.java
@@ -0,0 +1,15 @@
+package tr.edu.ozyegin.cs101.geometry2d;
+public class Point {
+    private double x;
+    private double y;
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double getX() {
+        return this.x;
+    }
+    public double getY() {
+        return this.y;
+    }
+}
